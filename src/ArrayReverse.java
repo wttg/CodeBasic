2fe8a3dd04e3d5d522cb0c902e86d46e58f24eef
wt <1240174666@qq.com>
2016-02-29 10:01:25 +0800

    code for interview

---
+++ src/ArrayReverse.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Created by wt on 2016/2/29.
+ * A way for Array Reverse
+ */
+public class ArrayReverse {
+    public static void main(String[] args) {
+        Integer[] list = new Integer[]{1, 3, 5, 7, 9};
+        Arrays.sort(list, Collections.reverseOrder());//Reverse Api
+        for (Integer i : list) {
+            System.out.print(i);
+            System.out.print(" ");
+        }
+    }
+}
---
+++ src/test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by wt on 2016/2/21.
+ */
+public class test {
+    public static void main(String[] args){
+        System.out.println("abc");
+    }
+}
