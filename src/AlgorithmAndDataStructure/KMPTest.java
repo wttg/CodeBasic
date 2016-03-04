7be0c20093e74d12d08f848845fccd59c968c3d7
wt <1240174666@qq.com>
2016-03-04 09:57:49 +0800

    code for interview

---
+++ src/AlgorithmAndDataStructure/KMPTest.java
@@ -0,0 +1,42 @@
+package AlgorithmAndDataStructure;
+
+/**
+ * Created by wt on 2016/3/3.
+ * KMP Algorithm
+ */
+public class KMPTest {
+    public int[] getNext(String b){
+        int len = b.length();
+        int j = 0;
+        int next[] = new int[len+1];
+        next[0] = next[1] = 0;
+        for(int i = 1;i < len;i++){
+            while (j > 0 && b.charAt(i) != b.charAt(j))
+                j = next[j];
+            if(b.charAt(i) == b.charAt(j))
+                j++;
+            next[i+1] = j;
+        }
+        return next;
+    }
+    public void search(String original,String sub,int[] next){
+        int j = 0;
+        for (int i = 0;i < original.length();i++){
+            while (j > 0 && original.charAt(i) != sub.charAt(j))
+                j = next[j];
+            if(original.charAt(i) == sub.charAt(j))
+                j++;
+            if(j == sub.length()){
+                System.out.println("find at position" + (i - j + 2));
+                System.out.println(original.subSequence(i - j + 1,i + 1));
+                j = next[j];
+            }
+        }
+    }
+    public static void main(String[] args){
+        KMPTest test = new KMPTest();
+        String original = "BBC ABCDABCDABDDABCDABDEABCDABD";
+        String sub = "ABCDABD";
+        test.search(original,sub,test.getNext(sub));
+    }
+}
