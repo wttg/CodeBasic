acf62eb35322ae18d9f2a7415c5edfac9c6c29c5
wt <1240174666@qq.com>
2016-05-03 21:21:32 +0800

    code for interview

---
+++ src/AlgorithmAndDataStructure/sort/QuickSort.java
@@ -0,0 +1,48 @@
+package AlgorithmAndDataStructure.sort;
+
+import java.util.Scanner;
+
+/**
+ * Created by wt on 2016/5/2.
+ */
+public class QuickSort {
+    int arr[];
+    public void quickSort(int left,int right){
+        if(left > right)
+            return;
+        int temp = arr[left]; //key
+        int i = left;
+        int j = right;
+        while (i != j){
+            while (arr[j] >= temp && i < j)//it must from right to left!!
+                j--;
+            while (arr[i] <= temp && i < j)
+                i++;
+            if(i < j){
+                int t = arr[i];
+                arr[i] = arr[j];
+                arr[j] = t;
+            }
+        }
+        arr[left] = arr[i];
+        arr[i] = temp;
+        quickSort(left,i-1);
+        quickSort(i+1,right);
+    }
+
+    public static void main(String args[]){
+        QuickSort quickSort = new QuickSort();
+
+        Scanner scanner = new Scanner(System.in);
+        int count = Integer.parseInt(scanner.nextLine());
+        quickSort.arr = new int[count];
+        for(int i = 0; i < count;i++){
+            quickSort.arr[i] = scanner.nextInt();
+        }
+
+        System.out.println(count);
+        quickSort.quickSort(0,count-1);
+        for (int i:quickSort.arr)
+            System.out.print(i + " ");
+    }
+}
