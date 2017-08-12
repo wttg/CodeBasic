f57c4d1818e44b6c6e3333feef60e0a1be577219
wt <1240174666@qq.com>
2017-08-12 16:08:43 +0800

    11111

--- src/wangyi/Main.java
+++ src/wangyi/Main.java
@@ -1,11 +1,31 @@
 package wangyi;
 
-/**
- * Created by wt on 2017/8/12.
- */
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
+        String str = in.next();
+
+        int times[]= new int[26];
+        for(int i = 0;i<times.length;i++)
+            times[i] = 0;
+        int count = 0;
+
+        for(int j=0;j<str.length();j++){
+            times[str.charAt(j)-'A']++;
+        }
+
+        for(int k=0;k<26;k++)
+            if(times[k]!=0)
+                count++;
+        if(count == 3)
+        System.out.println(0);
+        if(count == 1)
+            System.out.println(1);
+        if(count == 2)
+            System.out.println(2);
     }
 }
